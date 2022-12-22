e8098cdd1a0981edda7dad7a4a6afd8aeb4ed421
Marcial <27530602+MarcialBot@users.noreply.github.com>
2022-12-21 21:49:03 -0500

    Adds world node

--- Player.gd
+++ Player.gd
@@ -1,4 +1,23 @@
 extends KinematicBody2D
 
-func _ready():
-	pass # Replace with function body.
+# Use the `input` node to detect when the player is pressing a key
+onready var input = get_node("/root/Input")
+
+# The speed at which the player character moves
+var speed = 200
+
+# The direction the player is currently moving (1 for right, -1 for left)
+var direction = 1
+
+func _physics_process(delta):
+	# Check if the player is pressing the left arrow key
+	if input.is_action_pressed("ui_left"):
+		direction = -1
+		$Sprite.flip_h = true
+	# Check if the player is pressing the right arrow key
+	elif input.is_action_pressed("ui_right"):
+		direction = 1
+		$Sprite.flip_h = false
+	# If the player is not pressing any keys, stop moving
+	else:
+		direction = 0
